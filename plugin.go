f6080da0a86aeff16efc00288eb9d4640932bb50
Shin Sunyoung <s2017i30@e-mirim.hs.kr>
2021-09-18 17:08:17 +0900

    init

---
+++ plugin.go
@@ -0,0 +1,17 @@
+package root
+
+import (
+	"encoding/json"
+	"strings"
+
+	"github.com/mattermost/mattermost-server/v6/model"
+)
+
+//go:embed plugin.json
+var manifestString string
+
+var Manifest model.Manifest
+
+func init() {
+	_ = json.NewDecoder(strings.NewReader(manifestString)).Decode(&Manifest)
+}
